5f14ab12d4b78ad5b4980df71ddd68d8e30d74f5
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-18 18:42:08 +0100

    v2: add clean portal shell and routes

---
+++ v2/src/App.tsx
@@ -0,0 +1,30 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { OverviewPage } from './pages/OverviewPage';
+import { MasterDataPage } from './pages/MasterDataPage';
+import { IntakeReviewPage } from './pages/IntakeReviewPage';
+import './styles.css';
+
+export function App() {
+  return (
+    <div className="app-shell">
+      <aside className="sidebar">
+        <div className="brand">
+          <span className="brand-mark">SLH</span>
+          <div><strong>Transport Management</strong><small>V2 foundation</small></div>
+        </div>
+        <nav>
+          <NavLink to="/">Overview</NavLink>
+          <NavLink to="/master-data">Master Data</NavLink>
+          <NavLink to="/intake">Intake Review</NavLink>
+        </nav>
+      </aside>
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<OverviewPage />} />
+          <Route path="/master-data" element={<MasterDataPage />} />
+          <Route path="/intake" element={<IntakeReviewPage />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
