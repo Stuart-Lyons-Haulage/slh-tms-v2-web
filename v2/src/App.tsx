7f4251f257c7ddeb0425b57d286f21cd3992c56c
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-19 13:52:06 +0100

    feat(v2): route Planning and second-screen Pallet Control

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -2,6 +2,8 @@ import { NavLink, Route, Routes } from 'react-router-dom';
 import { OverviewPage } from './pages/OverviewPage';
 import { MasterDataPage } from './pages/MasterDataPage';
 import { IntakeReviewPage } from './pages/IntakeReviewPage';
+import { PlanningPage } from './pages/PlanningPage';
+import { PalletControlPage } from './pages/PalletControlPage';
 import './styles.css';
 
 export function App() {
@@ -16,6 +18,7 @@ export function App() {
           <NavLink to="/">Overview</NavLink>
           <NavLink to="/master-data">Master Data</NavLink>
           <NavLink to="/intake">Intake Review</NavLink>
+          <NavLink to="/planning">Planning</NavLink>
         </nav>
       </aside>
       <main className="content">
@@ -23,6 +26,8 @@ export function App() {
           <Route path="/" element={<OverviewPage />} />
           <Route path="/master-data" element={<MasterDataPage />} />
           <Route path="/intake" element={<IntakeReviewPage />} />
+          <Route path="/planning" element={<PlanningPage />} />
+          <Route path="/planning/pallet-control" element={<PalletControlPage />} />
         </Routes>
       </main>
     </div>
