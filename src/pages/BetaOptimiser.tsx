b678a29d230afda3ccdd7b677a9d63ed1f5a9088
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-21 12:37:05 +0100

    v2: remove retired legacy module

--- src/pages/BetaOptimiser.tsx
+++
@@ -1,9 +0,0 @@
-import { BetaRouteProposalReview } from "../components/BetaRouteProposalReview";
-import { BetaOptimiserCombined } from "./BetaOptimiserCombined";
-
-export function BetaOptimiser() {
-  return <>
-    <BetaRouteProposalReview />
-    <BetaOptimiserCombined />
-  </>;
-}
