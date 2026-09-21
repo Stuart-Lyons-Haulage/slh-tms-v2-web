1571a72e388cdfba61d8e1cfcc08382f72166731
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-21 12:35:40 +0100

    v2: align mobile navigation to core modules

--- src/components/MobileDock.tsx
+++ src/components/MobileDock.tsx
@@ -4,8 +4,8 @@ export function MobileDock({ openMenu }: { openMenu: () => void }) {
   return <nav className="mobile-dock" aria-label="Mobile TMS navigation">
     <NavLink to="/dashboard"><span>⌂</span><small>Dashboard</small></NavLink>
     <NavLink to="/" end><span>▦</span><small>Planner</small></NavLink>
-    <NavLink to="/planner-import"><span>⇧</span><small>Imports</small></NavLink>
-    <NavLink to="/driver-dispatch"><span>☷</span><small>Dispatch</small></NavLink>
+    <NavLink to="/pallet-control"><span>▤</span><small>Pallets</small></NavLink>
+    <NavLink to="/master-data"><span>≡</span><small>Master</small></NavLink>
     <button type="button" onClick={openMenu}><span>☰</span><small>More</small></button>
   </nav>;
 }
