3815e8ffdd612bbdde1a7c8c43d606d3efd9b2e9
danwilliams201302-max <daniel@lyonshaulage.com>
2026-09-24 21:20:54 +0100

    Add SLH Mobile to phone navigation

--- src/components/MobileDock.tsx
+++ src/components/MobileDock.tsx
@@ -2,10 +2,10 @@ import { NavLink } from 'react-router-dom';
 
 export function MobileDock({ openMenu }: { openMenu: () => void }) {
   return <nav className="mobile-dock" aria-label="Mobile TMS navigation">
+    <NavLink to="/mobile"><span>⌕</span><small>Mobile</small></NavLink>
     <NavLink to="/dashboard"><span>⌂</span><small>Dashboard</small></NavLink>
     <NavLink to="/" end><span>▦</span><small>Planner</small></NavLink>
     <NavLink to="/pallet-control"><span>▤</span><small>Pallets</small></NavLink>
-    <NavLink to="/master-data"><span>≡</span><small>Master</small></NavLink>
     <button type="button" onClick={openMenu}><span>☰</span><small>More</small></button>
   </nav>;
 }
